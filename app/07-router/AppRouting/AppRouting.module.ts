1f4b1e6d1c47dccf448a230e2e78c281a45bc942
Phil AND <you@example.com>
2018-10-03 17:27:43 +0200

    Comment

--- app/07-router/AppRouting/AppRouting.module.ts
+++ app/07-router/AppRouting/AppRouting.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home.component';
 import { ProductComponent } from './product.component';
 import { AboutComponent } from './about.component';
 
+// Configuration static des routes
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'product/:id', component: ProductComponent },
@@ -16,6 +17,7 @@ const routes: Routes = [
   { path: '**', redirectTo: '/' }
 ];
 
+// La configuration est passé au module RouterModule
 @NgModule({
   imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule],
