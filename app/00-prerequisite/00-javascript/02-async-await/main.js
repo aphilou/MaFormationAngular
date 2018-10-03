12fa030be9864bc9ec4b129f23989b17104acb16
Phil AND <you@example.com>
2018-10-03 08:08:58 +0100

    Plus de détails sur asynchrone

--- app/00-prerequisite/00-javascript/02-async-await/main.js
+++ app/00-prerequisite/00-javascript/02-async-await/main.js
@@ -6,8 +6,29 @@ async function sleep(time) {
     });
 }
 
+async function sleepWithError(time) {
+    return new Promise((resolve, reject) => {
+        if ( time % 2 === 0) {
+            reject("ERROR")
+        }
+        setTimeout(() => {
+            resolve("GOOD");
+        }, time);
+    });
+}
+
 (async () => {
     console.log('start');
     await sleep(1000);
     console.log('end');
+})();
+
+(async () => {
+    try {
+        console.log('aaaa');
+        const t = await sleepWithError(1000);
+        console.log('zzzz', t);
+    } catch (e) {
+        console.log('error', e);
+    }
 })();
