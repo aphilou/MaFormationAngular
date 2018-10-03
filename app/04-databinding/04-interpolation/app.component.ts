8a1dda0ed9a4570fdfcf1af1ec68b5054422f1fb
Phil AND <you@example.com>
2018-10-03 12:36:13 +0200

    Ajouter un observable

--- app/04-databinding/04-interpolation/app.component.ts
+++ app/04-databinding/04-interpolation/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -17,9 +18,10 @@ export class AppComponent {
 
   ngOnInit() {
     console.log('init the component');
-    setInterval(() => {
-      this.i++;
-      this.myName = `Angular ${this.i}`;
-    }, 1000);
+    // setInterval(() => {
+    //   this.i++;
+    //   this.myName = `Angular ${this.i}`;
+    // }, 1000);
+    Observable.interval(1000).map(n => n+1).subscribe(n => { this.myName = `Angular ${n}`; });
   }
 }
