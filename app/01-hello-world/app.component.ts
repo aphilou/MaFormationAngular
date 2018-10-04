a31d1a1bc4be4505fad1f249e338f385fa4f6ff4
Phil AND <you@example.com>
2018-10-04 11:44:24 +0200

    exemple de javascript ds interpollation

--- app/01-hello-world/app.component.ts
+++ app/01-hello-world/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<header class="toto">Hello {{name}}</header>`,
+  template: `<header class="toto">Hello {{getName()}}</header>`,
   styles: [ `.toto { color: red; }`]
 })
 export class AppComponent  { 
@@ -12,6 +12,10 @@ export class AppComponent  {
     setTimeout(() => {
       this.name = "Angular 5....";
     }, 2000);
+  }
+
+  getName() {
+    return this.name;
   }
 }
 
